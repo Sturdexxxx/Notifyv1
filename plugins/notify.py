138aa2ef7a697f9c531feb00dcfb8469796b80a7
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-08 22:20:40 +0530

    Update notify.py

--- plugins/notify.py
+++ plugins/notify.py
@@ -1,1 +1,46 @@
+import asyncio
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+from bot import Bot
+from config import ADMINS
+import requests as ree
+from bs4 import BeautifulSoup
+
+link1=[1]
+listlink=[1]
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('notify'))
+async def notify(client: Client, message: Message):
+    while True:
+        #kannada serials link
+        url = ('https://www.zee5.com/tv-shows/collections/before-tv-episodes-zee-kannada/0-8-670')
+
+        response = ree.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find the HTML element that contains information about the latest episode
+        episode_element = soup.find_all("a", class_="noSelect content", href=True)
+        
+        for l in episode_element:
+            if l['href'] not in listlink:
+                listlink.append(l['href'])
+                
+        #here we check the episode is new or not
+        new_link = set(listlink).difference(set(link1))
+        newlist=list(new_link)
+        for i in newlist:
+            link1.append(i)
+            b=str(i)
+            await client.send_message(message.chat.id, f"https://www.zee5.com{b}")
+            # print(f"https://www.zee5.com{b}\n\n")
+            await asyncio.sleep(3)
+            
+        #here we delete the old episode link(premium free)
+        duplicatelinks = set(link1).difference(set(listlink))
+        duplicatelist=list(duplicatelinks)
+        for j in duplicatelist:
+            link1.remove(j)
+        notify(client,message)
+        break
+
 
