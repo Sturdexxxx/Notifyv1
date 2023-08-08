339f77dbb5bb96482c65d3a4f0653220dc71ada4
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-08 22:03:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiohttp import web
 from plugins import web_server
 
 import pyromod.listen
-from pyrogram import Client
+from pyrogram import Client, filters
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
