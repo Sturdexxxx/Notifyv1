1ccb924e8ec8d76aa6a3a3498c1e54cf549e55c5
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-10 20:39:40 +0530

    Update notify.py

--- plugins/notify.py
+++ plugins/notify.py
@@ -1,7 +1,7 @@
 import asyncio
 from pyrogram import filters
-from pyrogram import Client
-from pyrogram.types import Message
+from pyrogram import Client as client
+from pyrogram.types import Message as message
 import time
 from bot import Bot
 from config import ADMINS
@@ -10,44 +10,42 @@ from bs4 import BeautifulSoup
 
 link1=[1]
 listlink=[1]
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('notify'))
-async def notify(client: Client, message: Message):
-#     noti()
-        while True:
-                print("Searching..??")
+
+while True:
+        print("Searching..??")
                 #kannada serials link
-                url = ('https://www.zee5.com/tv-shows/collections/before-tv-episodes-zee-kannada/0-8-670')
+        url = ('https://www.zee5.com/tv-shows/collections/before-tv-episodes-zee-kannada/0-8-670')
         
-                response = ree.get(url)
-                soup = BeautifulSoup(response.content, 'html.parser')
+        response = ree.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
         
                 # Find the HTML element that contains information about the latest episode
-                episode_element = soup.find_all("a", class_="noSelect content", href=True)
+        episode_element = soup.find_all("a", class_="noSelect content", href=True)
                 
-                for l in episode_element:
-                    if l['href'] not in listlink:
-                        listlink.append(l['href'])
+        for l in episode_element:
+                if l['href'] not in listlink:
+                listlink.append(l['href'])
                         
                 #here we check the episode is new or not
-                new_link = set(listlink).difference(set(link1))
-                newlist=list(new_link)
-                for i in newlist:
-                    link1.append(i)
-                    b=str(i)
-                    a = None
-                    a = await client.send_message(message.chat.id, f"https://www.zee5.com{b}")
-                    if a:
-                        print('Msg sent successfully..!')
-                    else:
-                        print('Msg not sent successfully..!')
+        new_link = set(listlink).difference(set(link1))
+        newlist=list(new_link)
+        for i in newlist:
+                link1.append(i)
+                b=str(i)
+                a = None
+                a = await client.send_message(message.chat.id, f"https://www.zee5.com{b}")
+                if a:
+                    print('Msg sent successfully..!')
+                else:
+                    print('Msg not sent successfully..!')
                     # print(f"https://www.zee5.com{b}\n\n")
-                    time.sleep(3)
+                time.sleep(3)
             
                 #here we delete the old episode link(premium free)
-                duplicatelinks = set(link1).difference(set(listlink))
-                duplicatelist=list(duplicatelinks)
-                for j in duplicatelist:
-                    link1.remove(j)
-                time.sleep(10)
-                await notify(client,message)
-                break
+        duplicatelinks = set(link1).difference(set(listlink))
+        duplicatelist=list(duplicatelinks)
+        for j in duplicatelist:
+                link1.remove(j)
+        time.sleep(10)
+        #await notify(client,message)
+        continue
