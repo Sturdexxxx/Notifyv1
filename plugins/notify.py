6a1963cf89cdf91ecebb09597d682f57eac84224
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-13 18:16:14 +0530

    Update notify.py

--- plugins/notify.py
+++ plugins/notify.py
@@ -12,9 +12,7 @@ from bs4 import BeautifulSoup
 
 @Bot.on_message(filters.private & filters.command(["start"]))
 async def start_command(client: Client, message: Message):
-    d = await client.send_message(chat_id = message.chat.id, text = "I am alive 💥")
-    await time.sleep(60)
-    await d.delete()
+    await client.send_message(chat_id = message.chat.id, text = "I am alive 💥")
 
 link1=[1]
 listlink=[1]
