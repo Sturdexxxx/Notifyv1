fde8e9646a03992d418c404e5d1232262b413120
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-13 21:54:34 +0530

    Update notify.py

--- plugins/notify.py
+++ plugins/notify.py
@@ -20,41 +20,46 @@ async def newepisode(client: Client, message: Message):
         print("Searching..??")
         #kannada serials link
         url = ('https://www.zee5.com/tv-shows/collections/before-tv-episodes-zee-kannada/0-8-670')
-        
         response = ree.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
         
         # Find the HTML element that contains information about the latest episode
         episode_element = soup.find_all("a", class_="noSelect content", href=True)
                 
-        for l in episode_element:
-                if l['href'] not in listlink:
-                    listlink.append(l['href'])
+        for i in episode_element:
+            u = "https://www.zee5.com"+str(i['href'])
+            r = ree.get(u)
+            s = BeautifulSoup(r.content, 'html.parser')
+            # Find the HTML element that contains information about the latest episode
+            episode_no = s.find("div", class_="metaInfo lineHeightClass")
+            episode = str(episode_no.find("p"))
+            ep=str(re.findall("E\s\d+",episode)[0])
+            d[ep]=u
+            list2.append(ep)
+        
         #here we check the episode is new or not
-        new_link = set(listlink).difference(set(link1))
-        newlist=list(new_link)
-        #c = await client.send_message(LOG_ID, f"{len(newlist)} \n\n{newlist}")
-        for i in newlist:
-                link1.append(i)
-                b=str(i)
-                a = None
-                a = await client.send_message(chat_id = 5963138883, text = f"https://www.zee5.com{b}")
-                d = await client.send_message(chat_id = 1284476297, text = f"https://www.zee5.com{b}")
-                if a and d:
-                    print('Msg sent successfully to both..!')
-                elif a:
-                    print('Msg sent successfully to Monstar..!')
-                elif d:
-                    print('Msg sent successfully to Nandan..!')
-                else:
-                    print('Msg not sent successfully..!')
-                time.sleep(5)
+        s3=set(list2).difference(set(list1))
+        new_list = list(s3)
+        for i in new_list:
+            list1.append(i)
+            a=str(i)
+            a = None
+            a = await client.send_message(chat_id = 5963138883, text = f"https://www.zee5.com{b}")
+            d = await client.send_message(chat_id = 1284476297, text = f"https://www.zee5.com{b}")
+            if a and d:
+                print('Msg sent successfully to both..!')
+            elif a:
+                print('Msg sent successfully to Monstar..!')
+            elif d:
+                print('Msg sent successfully to Nandan..!')
+            else:
+                print('Msg not sent successfully..!')
+            time.sleep(5)
             
         #here we delete the old episode link(premium free)
-        duplicatelinks = set(link1).difference(set(listlink))
-        duplicatelist=list(duplicatelinks)
-        for j in duplicatelist:
-                link1.remove(j)
+        duplicate=set(list1).difference(set(list2))
+        duplicate_ep=list(duplicate)
+        for j in duplicate_ep:
+                list1.remove(j)
         time.sleep(20)
-        #await c.delete()
         continue
