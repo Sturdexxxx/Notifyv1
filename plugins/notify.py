1993f555d172636caec9a8603504356614c89816
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-13 12:23:43 +0530

    Update notify.py

--- plugins/notify.py
+++ plugins/notify.py
@@ -4,14 +4,40 @@ from pyrogram import Client as client
 from pyrogram.types import Message as message
 import time
 from bot import Bot
-from config import ADMINS
+from config import ADMINS, METHOD_MESSAGE
 import requests as ree
 from bs4 import BeautifulSoup
 
 link1=[1]
 listlink=[1]
 
-while True:
+LINK_REPLY_MARKUP = InlineKeyboardMarkup(
+    [
+            InlineKeyboardButton(
+                "New Episodes", callback_data="newepisode"
+            ),
+            InlineKeyboardButton(
+                "Stop Searching", callback_data="stopsearch"
+            ),
+    ]
+)
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command(["link"]))
+async def date(bot, message):
+    method_name = query.data.split("#")[1]
+    REPLY_MARKUP = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Back", callback_data="LINK_REPLY_MARKUP")]]
+        )
+    s = METHOD_MESSAGE.format(method=method_name)
+    return await query.message.edit(s, reply_markup=METHOD_REPLY_MARKUP)
+        
+    await query.message.edit(
+            "Method changed successfully",
+            reply_markup=REPLY_MARKUP,
+        )
+
+@Bot.on_callback_query(filters.regex('newepisode'))
+async def newepisode(client: Client, message: Message):
+    while True:
         print("Searching..??")
                 #kannada serials link
         url = ('https://www.zee5.com/tv-shows/collections/before-tv-episodes-zee-kannada/0-8-670')
@@ -33,7 +59,7 @@ while True:
                 link1.append(i)
                 b=str(i)
                 a = None
-                a = client.send_message(message.chat.id, f"https://www.zee5.com{b}")
+                a = await client.send_message(message.chat.id, f"https://www.zee5.com{b}")
                 if a:
                     print('Msg sent successfully..!')
                 else:
@@ -49,3 +75,11 @@ while True:
         time.sleep(10)
         #await notify(client,message)
         continue
+    
+@Bot.on_callback_query(filters.regex('stopsearch'))
+async def stopsearch():
+    pass
+        
+# @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
+# async def batch(client: Client, message: Message):
+#         await client.send_message(chat_id=message.chat.id, text=f"<b>{}</b>")
