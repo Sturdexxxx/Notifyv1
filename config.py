97852fdb2605ea706883f88774008e60f9105679
Sturdexxxx <134261890+Sturdexxxx@users.noreply.github.com>
2023-10-30 14:41:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,71 +1,34 @@
-#(©)CodeXBotz
 from os import environ
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-#Bot token @Botfather
-# TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5872747581:AAH7_XPCOCEVfbgUhepjJWlcOmj8wjDTjBk") #url_v3
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5417821247:AAFVtQDdpcnXcCt7aWQdBcl13ABuvZlOtXg") #drm
-
-#Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "3393749"))
-
-#Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "a15a5954a1db54952eebd08ea6c68b71")
-
-#Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001693231644"))
-
-# #Your log channel Id
 LOG_ID = int(os.environ.get("LOG_ID", "-1001956515516"))
-
-#OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5531584953"))
-
-
-#Port
 PORT = os.environ.get("PORT", "8080")
-
-#Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Jayanna:Jayanna2023@yash.tm1c2bd.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "New_Divya_Spandana")
-
-# #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001239856060"))
-
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "10"))
-
-# #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI store private files for @EclipseAnime and other users can access it from special link.")
+
 try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5531584953 1943966786 5395870942").split()):
+    ADMINS = []
+    for x in os.environ.get("ADMINS", "5531584953 1943966786 5395870942").split():
         ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+    raise Exception("Your Admins list does not contain valid integers.")
 
-#Force sub message 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
-
-# #set your Custom Caption here, Keep None for Disable Custom Caption
-# CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b><code>{filename}</code> \n\n𝐉𝐨𝐢𝐧 -> <a href='https://telegram.dog/link_serials'>𝐂𝐡𝐚𝐧𝐧𝐞𝐥</a></b>")
-
-# #shortner
-# SHORTLINK_URL = environ.get('SHORTLINK_URL', 'omegalinks.in')
-# SHORTLINK_API = environ.get('SHORTLINK_API', '0af17d003ce7020eb85d2a54fe0f6ef50e770e40')
-
-# #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
-
-# #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
-
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
-
 ADMINS.append(OWNER_ID)
-
 LOG_FILE_NAME = "filesharingbot.txt"
 
 logging.basicConfig(
@@ -80,15 +43,13 @@ logging.basicConfig(
          ),
          logging.StreamHandler()
      ]
- )
- logging.getLogger("pyrogram").setLevel(logging.WARNING)
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
 
- def LOGGER(name: str) -> logging.Logger:
-     return logging.getLogger(name)
-
-
- LOG_TEXT_P =  """#𝐍𝐞𝐰𝐔𝐬𝐞𝐫𝐊𝐒
- <b>᚛› 𝐈𝐃 - <code>{}</code></b>
- <b>᚛› 𝐍𝐚𝐦𝐞 - {}</b>
- """
+LOG_TEXT_P = """#𝐍𝐞𝐰𝐔𝐬𝐞𝐫𝐊𝐒
+<b>᚛› 𝐈𝐃 - <code>{}</code></b>
+<b>᚛› 𝐍𝐚𝐦𝐞 - {}</b>
+"""
